3e1d41290d1fae95ffa1b91a8e87fc4dcbf53026
Martin Honnen <martin.honnen@gmx.de>
2026-05-13 11:51:57 +0200

    Projektdateien hinzufügen.

---
+++ Schxslt2TranspilationAndValidationTest1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using PhoenixmlDb.Xslt;
+
+var transpileLocation = @"C:\Users\marti\OneDrive\Documents\schematron\schxslt2-1.10.3\transpile-indent-report.xsl"; // @"C:\Users\marti\source\repos\PhoenixmlWorkbench\PhoenixmlWorkbench\wwwroot\examples\xslt\schematron\schxslt2\transpile.xsl";
+
+var schematronFile = args[0];
+
+var instanceFile = args[1];
+
+//Console.WriteLine(schematronFile);
+
+//Console.WriteLine(instanceFile);
+
+var transformer = new XsltTransformer();
+//transformer.PreloadedResources
+await transformer.LoadStylesheetAsync(await File.ReadAllTextAsync(transpileLocation), new Uri(transpileLocation));
+
+transformer.SetSourceDocumentUri(new Uri(schematronFile));
+
+var transpiledSchematron = await transformer.TransformAsync(await File.ReadAllTextAsync(schematronFile));
+
+
+transformer = new XsltTransformer();
+//transformer.PreloadedResources
+await transformer.LoadStylesheetAsync(transpiledSchematron, new Uri(transpileLocation));
+
+transformer.SetSourceDocumentUri(new Uri(instanceFile));
+
+var svrlReport = await transformer.TransformAsync(await File.ReadAllTextAsync(instanceFile));
+
+Console.WriteLine(svrlReport);
